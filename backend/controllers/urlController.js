8915a9f22c1161627d55cda3fccdd8e2c622aeb7
Syed Abid Ahmed <syeds@Syeds-MacBook-Air.local>
2025-08-27 12:06:14 +0530

    REST API for URLs

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -1,25 +1,69 @@
 const asyncHandler = require('express-async-handler')
-
+const Url = require('../models/urlModel')
+const { nanoid } = require("nanoid"); 
+const baseUrl = process.env.BASE_URL;
 
 const getUrl = asyncHandler(async (req,res) => {
-    res.send("this is a get req");
-})
-const postUrl = asyncHandler(async (req,res) => {
-    const orgUrl = req.body?.url ;
-    if(!orgUrl) {
-        res.status(400)
-        throw new Error("Please add a URL")
-    }
-    
-    res.send(`this is a post req for ${orgUrl}`);
+    const urls = await Url.find()
+    res.json(urls);
 })
 
+const postUrl = asyncHandler(async (req, res) => {
+  const url = req.body?.url;
+
+  if (!url) {
+    res.status(400);
+    throw new Error("Please add a URL");
+  }
+
+    let existing = await Url.findOne({ longUrl });
+    if (existing) {
+      return res.status(200).json({
+        message: "URL already shortened",
+        longUrl: existing.longUrl,
+        shortUrl: `${baseUrl}/${existing.shortUrl}`,
+      });
+    }
+
+    const shortCode = nanoid(7);
+
+    const newUrl = await Url.create({
+      longUrl,
+      shortUrl: shortCode,
+    });
+
+    res.status(201).json({
+      message: "URL shortened successfully",
+      longUrl: newUrl.longUrl,
+      shortUrl: `${baseUrl}/${newUrl.shortUrl}`
+    });
+});
+
+
 const patchUrl = asyncHandler(async (req,res) => {
-    res.send("this is a patch req");
+    const url = await Url.findById(req.params.id)
+    
+    if(!url) {
+        res.status(400)
+        throw new error('URL Not Found!')
+    }
+
+    const updatedUrl = await Url.findByIdAndUpdate(req.params.id,req.body,{
+        new: true, 
+    })
+    res.status(200).json(updatedUrl);
 })
 
 const deleteUrl = asyncHandler(async (req,res) => {
-    res.send("this is a delete req");
+    const url = await Url.findById(req.params.id)
+
+    if(!url) {
+        res.status(400)
+        throw new error('URL Not Found!')
+    }
+
+    await url.deleteOne()
+    res.status(200).json({id: req.params.id});
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDb = require('./config/db')
 const urlRoutes = require('./routes/urlRoutes');
 const { errorHandler } = require('./middleware/errorMiddleware')
+
+connectDb();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
